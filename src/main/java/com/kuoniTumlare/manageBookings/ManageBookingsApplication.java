087713f48597e9cda87b8bec17db34de55a53840
Ankit Gupta <ankit.gupta@kuonitumlare.com>
2025-12-19 11:59:29 +0530

    Project setup

---
+++ src/main/java/com/kuoniTumlare/manageBookings/ManageBookingsApplication.java
@@ -0,0 +1,13 @@
+package com.kuoniTumlare.manageBookings;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManageBookingsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ManageBookingsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kuoniTumlare/manageBookings/ManageBookingsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kuoniTumlare.manageBookings;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManageBookingsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
